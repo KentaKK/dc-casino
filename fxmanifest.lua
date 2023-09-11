905e3c2341e1f8c5d4cb2769645454b7f855377d
Kenta <102387086+KentaKK@users.noreply.github.com>
2023-09-11 18:40:20 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
-
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
 author 'Disabled Coding'
 description 'A casino resource for which you do not have to sell a kidney for'
 version '1.0.0'
 repository 'https://github.com/Disabled-Coding/dc-casino'
 
 shared_scripts {
+    '@es_extended/imports.lua',
     '@ox_lib/init.lua',
     'configs/*.lua'
 }
@@ -18,6 +20,3 @@ server_scripts {
     'server/slots.lua',
     'server/roulette.lua',
 }
-
-lua54 'yes'
-use_experimental_fxv2_oal 'yes'
